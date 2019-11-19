3c25f6af2305780b175a79ca615582d694525b90
Kensuits <48896822+Kensuits@users.noreply.github.com>
2019-11-18 19:00:57 -0500

    Add files via upload

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,31 +1,17 @@
 package craps_project;
 
-//import static org.junit.Assert.*;
-//import static org.junit.Assume.assumeNoException;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class PlayerTest {
-    public static String name;
-    static InputStreamReader ir = new InputStreamReader(System.in);
-    static BufferedReader br = new BufferedReader(ir);
-    
+
 	@Test
-	public void Player() {
-	}
-	
-	public static String playerId() {
-		try {
-			System.out.print("Enter Player Id = ");
-			name = br.readLine();
-		}catch(Exception e) {
-			System.out.println("Invalid Entry");
-		}
-		return name;
+	public void playerTest() {
 		
+		String actual = Player.name;
+		String expected = "Kenny";
+		assertNotNull(actual, expected);
 	}
 
 }
